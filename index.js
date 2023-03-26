e10935b211a27302d4fd71e1d34c66f9808d5df2
SyedAli017 <124472340+SyedAli017@users.noreply.github.com>
2023-03-26 17:04:06 +0530

    improved code for JavaScript

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const inputText = document.getElementById("input-text");
 const inputBtn = document.getElementById("input-btn");
 const ulSection = document.getElementById("ul-section");
 
-let leads = [];
+let myLeads = [];
 
-inputBtn.addEventListener("click", function () {
-  leads.push(inputText.value);
-  inputText.value = "";
-  showLeads();
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+const localStorageLeads = JSON.parse(localStorage.getItem("myLeads"));
+
+if (localStorageLeads) {
+  myLeads = localStorageLeads;
+  show(myLeads);
+}
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    show(myLeads);
+  });
 });
 
-function showLeads() {
+function show(leads) {
   let leadsList = "";
-  for (i in leads) {
+  for (i in myLeads) {
     leadsList += `
             <li>
                 <a target='_blank' href='${leads[i]}'>
@@ -21,6 +32,18 @@ function showLeads() {
             </li>
         `;
   }
-
   ulSection.innerHTML = leadsList;
 }
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputText.value);
+  inputText.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  show(myLeads);
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  show(myLeads);
+});
